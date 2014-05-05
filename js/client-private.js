94223bf63f3d41ee0bae086dc282ea27ef8bcc2e
Matthew Hancock <mbhancock@me.com>
2014-05-05 12:40:52 -0400

    Added comments

--- js/client-private.js
+++ js/client-private.js
@@ -4,16 +4,19 @@ var to_user = "";
 $(document).ready(function() {
 	parseURL(location.search);
 
+	// Initialize the window
 	$('#name').text("Welcome, " + username);
 	$('#input').attr("placeholder", "Press 'Enter' or click 'SEND' to send " + to_user + " a message");
 	$('#input').focus();
 
+	// Prevent default POST action and register our send function
 	$('#sendform').submit(function(event)
 	{
 		event.preventDefault();
 		sendMessage();	
 	}); 
 
+	// Inline (emoticon) button
 	$('#smile').hover(function()
 	{
 		$(this).attr('src','images/smile_hover.png');
@@ -22,6 +25,7 @@ $(document).ready(function() {
       $(this).attr('src','images/smile.png')
     });
 
+	// Upload image button
     $('#camera').hover(function()
 	{
 		$(this).attr('src','images/camera_hover.png');
@@ -35,6 +39,7 @@ $(document).ready(function() {
     	$('#upload').click();
     });
 
+    // Send emoticon button
     $('#smile').click(function()
     {
     	$('#popover').fadeToggle(200);
@@ -46,6 +51,7 @@ $(document).ready(function() {
     	$('#input').focus();
     });
 
+    // Send image button
     $('#upload').change(function(e)
     {
     	var file = e.target.files[0];
@@ -53,33 +59,35 @@ $(document).ready(function() {
     });
 });
 
-function handleFiles(files)
-{
-	alert(files);
-}
-
+// Parse URL
 function parseURL(query)
 {
+	// Get the username and who to send to from URL
 	args = query.split("&");
 	username = args[0].split("=")[1];
 	to_user = args[1].split("=")[1];
 }
 
+// Log the user out
 function logout()
 {
 	window.close();
 }
 
+// Send a message
 function sendMessage()
 {
+	// Get the message contents
 	var input = $('#input');
 	if(input.val() != "" || input.val() == 'undefined')
 	{
+		// Check for valid length
 		var message = chunkMessage(input.val());
 		if(message == false)
 			alert("Message length must be less than 99");
 		else
 		{
+			// Send the message
 			var command = "SEND " + to_user + "|" + message;
 			window.opener.commandFromChild(command);
 			input.val('');
@@ -90,6 +98,7 @@ function sendMessage()
 	}
 }
 
+// Send emoticon
 function sendEmoticon(url)
 {
 	window.opener.commandFromChild("SEND " + to_user + "|" + "emoticon:" + url);
@@ -98,6 +107,7 @@ function sendEmoticon(url)
 	$('#messages').scrollTop($('#messages')[0].scrollHeight);
 }
 
+// Send iamge file
 function sendImage(file)
 {
 	var fr = new FileReader();
@@ -115,47 +125,62 @@ function sendImage(file)
 	fr.readAsDataURL(file);
 }
 
+// Return data from parent window
 function receiveFromParent(user, message)
 {
 	$('#messages').append(makeMessageFrom(user, message));
 }
 
+// Show a received message from the current user
 function makeMessageSelf(message)
 {
 	var html = '<div class="message">';
 	html += '<img class="avatar_self" src="images/avatar1.png"  alt="' + username + '">';
 	html = html + '<div class="from_user_self">' + username + '</div>';
+
+	// Show emoticon message
 	if(message.substring(0,9) == "emoticon:")
 	{
 		var image = message.substring(9);
 		html = html + '<img src="images/emoticons/' + image + '" class="emoticon_self_message">';
 	}
+
+	// Show image message
 	else if(message.substring(0,7) == "upload:")
 	{
 		var imageURI = message.substring(7);
 		html = html + '<img scr="' + imageURI + '" class="image_self_message">';
 	}
+
+	// Show text message
 	else
 		html = html + '<div class="from_message_self">' + message + '</div>';
 	html += "</div>";
 	return html;
 }
 
+// Show message received from another user
 function makeMessageFrom(user, message)
 {
 	var html = '<div class="message">';
 	html += '<img class="avatar" src="images/avatar2.png"  alt="' + user + '">';
 	html = html + '<div class="from_user">' + user + '</div>';
+
+	// Show emoticon message
 	if(message.substring(0,9) == "emoticon:")
 	{
 		var image = message.substring(9);
 		html = html + '<img src="images/emoticons/' + image + '" class="emoticon_message">';
 	}
+
+	// Show image message
 	else if(message.substring(0,7) == "upload:")
 	{
 		var imageURI = message.substring(7);
 		html = html + '<img scr="' + imageURI + '" class="image_message">';
 	}
+
+	// Show text message
 	else
 		html = html + '<div class="from_message">' + message + '</div>';
 	html += '</div>';
--- js/client.js
+++ js/client.js
@@ -1,28 +1,36 @@
+// If Firefox, use MozWebSocket
 if(this.MozWebSocket)
 {
 	WebSocket = MozWebSocket;
 }
+
+// Globals
 var users = {};
 var username = "";
 var signin = true;
 
+// Chat Server url
 var url = "ws://localhost:8787/chat";
 
+// On ready function
 $(document).ready(function() {
 	$('#username').focus();
 
+	// Prevent default POST action and register our login function
 	$('#loginform').submit(function(event)
 	{
 		event.preventDefault();
 		login();	
 	});
 
+	// Prevent default POST action and register our send function
 	$('#sendform').submit(function(event)
 	{
 		event.preventDefault();
 		sendMessage();	
 	});
 
+	// Inline (emoticon) button
 	$('#smile').hover(function()
 	{
 		$(this).attr('src','images/smile_hover.png');
@@ -31,6 +39,7 @@ $(document).ready(function() {
       $(this).attr('src','images/smile.png')
     });
 
+	// Upload image button
     $('#camera').hover(function()
 	{
 		$(this).attr('src','images/camera_hover.png');
@@ -49,12 +58,14 @@ $(document).ready(function() {
     	$('#popover').fadeToggle(200);
     });
 
+    // Send emoticon button
     $('.emoticon').click(function()
     {
     	sendEmoticon($(this).attr('src').split("/")[2]);
     	$('#input').focus();
     });
 
+    // Send image button
     $('#upload').change(function(e)
     {
     	var file = e.target.files[0];
@@ -62,63 +73,86 @@ $(document).ready(function() {
     });
 });
 
+// Connect to the websocket server
 function makeConnections()
 {
 	window.socket = new WebSocket(url);
+
+	// Attempt to login
 	window.socket.onopen = function()
 	{
 		window.socket.send("ME IS " + username + "|");
 	}
 
+	// Message recv handler
 	window.socket.onmessage = function(msg)
 	{
 		var whole_message = msg.data.toString();
+
+		// If the user needs to be signed in still, parse ME IS response
 		if(signin)
 		{
 			whole_message = whole_message.split("|")[0];
+
+			// Login error
 			if(whole_message == "ERROR")
 			{
 				$('#error').fadeIn(400);
 				$('#username').focus();
 				window.socket.close()
 			}
+
+			// Successful login
 			else
 			{
 				users[username] = 1;
 				signin = false;
+
+				// Show the main interface
 				$('#login').fadeOut(400, function()
 				{
 					$('#chat').fadeIn(400);
 					$('#name').text("Welcome, " + username);
 					$('#input').focus();
 
+					// Automatically refresh the users list every 2s
 					update();
 					window.ID = window.setInterval(function(){update();}, 2000);
 				});
 			}
 		}
+
+		// Receive a broadcast
 		else if(whole_message.substring(0,9) == "BROADCAST")
 		{
 			var parts = whole_message.split("|");
 			var from_user = parts[0].split(" ")[2];
 			var message = parts[1];
 
+			// Add the recv'd message to the interface
 			if(from_user == username)
 				$('#messages').append(makeMessageSelf(message));
 			else
 				$('#messages').append(makeMessageFrom(from_user, message));
 
 
+			// Scoll down to the most recent message
 			$('#messages').scrollTop($('#messages')[0].scrollHeight);
 		}
+
+		// Rceive a private message
 		else if(whole_message.substring(0,7) == "PRIVATE")
 		{
 			var parts = whole_message.split("|");
 			var from_user = parts[0].split(" ")[2];
 			var starting = 14 + from_user.length;
 			var message = parts[1];
+
+			// Send the message to a separate child window
 			sendToChild(from_user, message);
 		}
+
+		// Receive a list of users
 		else
 		{
 			whole_message = whole_message.split("|")[0];
@@ -138,14 +172,20 @@ function makeConnections()
 	}
 }
 
+
+// Login function
 function login()
 {
 	var name = $('#username').val();
+
+	// No username given
 	if(name == "")
 	{
 		$('#error').fadeIn(400);
 		$('#username').focus();
 	}
+
+	// Attempt to use the entered username
 	else
 	{
 		username = name;
@@ -154,10 +194,13 @@ function login()
 	}
 }
 
+// Log the user out
 function logout()
 {
+	// Stop updating user list
 	clearInterval(window.ID);
 	
+	// Hide the main interface
 	$('#error').hide();
 	$('#chat').fadeOut(400, function()
 	{
@@ -166,20 +209,26 @@ function logout()
 		$('#username').focus();
 		signin = true;
 		$('#messages').empty();
+
+		// Close the socket
 		window.socket.close();
 	});
 }
 
+// Send a message
 function sendMessage()
 {
+	// Get the message contents
 	var input = $('#input');
 	if(input.val() != "" || input.val() == 'undefined')
 	{
+		// Check for valid length
 		var message = chunkMessage(input.val());
 		if(message == false)
 			alert("Message length must be less than 99");
 		else
 		{
+			// Send the message
 			window.socket.send("BROADCAST|" + message);
 
 			input.val('');
@@ -188,12 +237,14 @@ function sendMessage()
 	}
 }
 
+// Send emoticon
 function sendEmoticon(url)
 {
 	window.socket.send("BROADCAST|emoticon:" + url);
 	$('#smile').click();
 }
 
+// Send iamge file
 function sendImage(file)
 {
 	var fr = new FileReader();
@@ -208,43 +259,57 @@ function sendImage(file)
 	fr.readAsDataURL(file);
 }
 
+// Show a received message from the current user
 function makeMessageSelf(message)
 {
 	var html = '<div class="message">';
 	html += '<img class="avatar_self" src="images/avatar1.png"  alt="' + username + '">';
 	html = html + '<div class="from_user_self">' + username + '</div>';
+
+	// Show emoticon message
 	if(message.substring(0,9) == "emoticon:")
 	{
 		var image = message.substring(9);
 		html = html + '<img src="images/emoticons/' + image + '" class="emoticon_self_message">';
 	}
+
+	// Show image message
 	else if(message.substring(0,7) == "upload:")
 	{
 		var imageURI = message.substring(7);
 		html = html + '<img scr="' + imageURI + '" class="image_self_message">';
 	}
+
+	// Show text message
 	else
 		html = html + '<div class="from_message_self">' + message + '</div>';
 	html += "</div>";
 	return html;
 }
 
+// Show message received from another user
 function makeMessageFrom(user, message)
 {
 	var pic_num = users[user];
 	var html = '<div class="message" onclick="openWindow(\'' + user + '\')">';
 	html += '<img class="avatar" src="images/avatar' + pic_num + '.png"  alt="' + user + '">';
 	html = html + '<div class="from_user">' + user + '</div>';
+	
+	// Show emoticon message
 	if(message.substring(0,9) == "emoticon:")
 	{
 		var image = message.substring(9);
 		html = html + '<img src="images/emoticons/' + image + '" class="emoticon_message">';
 	}
+
+	// Show image message
 	else if(message.substring(0,7) == "upload:")
 	{
 		var imageURI = message.substring(7);
 		html = html + '<img scr="' + imageURI + '" class="image_message">';
 	}
+
+	// Show text message
 	else
 		html = html + '<div class="from_message">' + message + '</div>';
 	html += '</div>';
@@ -263,16 +328,21 @@ function chunkMessage(message)
 	}
 }
 
+// Send an update request
 function update()
 {
 	window.socket.send("WHO HERE");
 }
 
+// Update the users list
 function executeUpdate(names)
 {
+	// Split the returned usernames by comma
 	var returned = (names.split("|")[0]).split(",");
 	var counter = 2;
 	$('#users').empty();
+
+	// Add each name to the users list
 	$.each(returned, function(i, name) {
 		if(name != username)
 		{
@@ -295,27 +365,36 @@ function executeUpdate(names)
 	});
 }
 
+// Received a send from private message window
 function commandFromChild(command)
 {
 	window.socket.send(command);
 }
 
+// Send a message to a private window
 function sendToChild(user, message)
 {
+	// Open (or focus) private message window
 	var w = openWindow(user, true);
 	var win = w[0];
+
+	// If the window wasn't open, wait for it to load then send
 	if(w[1] == false)
 	{
 		$(win).load(function() {
 			win.receiveFromParent(user, message);
 		});
 	}
+
+	// Window already open, send the message
 	else
 	{
 		win.receiveFromParent(user, message);
 	}
 }
 
+
+// Open a private message window or focus existing window
 function openWindow(user, find)
 {
 	if(typeof find == 'undefined')
@@ -323,11 +402,13 @@ function openWindow(user, find)
 		find = false;
 	}
 
+	// Maintain an array of open windows
 	if(typeof openWindow.winRefs == 'undefined')
 	{
 		openWindow.winRefs = new Array();
 	}
 
+	// Window not already open, open a new window
 	if(typeof openWindow.winRefs[user] == 'undefined' || openWindow.winRefs[user].closed)
 	{
 		var url = 'client-private.html' + '?username=' + username + '&touser=' + user;
@@ -336,6 +417,8 @@ function openWindow(user, find)
 		if(find)
 			return [openWindow.winRefs[user], false];
 	} 
+
+	// Window exists, focus it
 	else 
 	{
 		window.open('', user).focus();
